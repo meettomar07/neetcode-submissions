873bdc7d20c07817cd0c5c28bccf8fcff8f02df0
Meet Tomar <b24bs2201@iitj.ac.in>
2026-07-16 21:30:30 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = {}
+        for letter in s:
+            if letter in count:
+                count[letter] += 1
+            else:
+                count[letter] = 1
+        for letter in t:
+            if letter not in count:
+                return False
+            count[letter] -= 1
+            if count[letter] < 0:
+                return False
+        return True
+            
